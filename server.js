23d41cef3be27deed68b04cd439babd6935959c5
Rane <rane@blasph.me>
2019-06-11 18:54:46 -0700

    initialized knex

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ server.use(express.json());
 server.use(heltmet());
 server.use(logger('dev'));
 
+server.get('/', (req, res) => {
+   res.send("<h1>Hello, it works.</h1>");
+});
+
 module.exports = server;
